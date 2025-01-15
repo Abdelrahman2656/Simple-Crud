7ff14f12c4a882eba4aaea83c0cc4d8583b753e7
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-01-15 04:02:24 +0200

    updated

--- public/JavaScript/client.js
+++ public/JavaScript/client.js
@@ -6,7 +6,9 @@ function getProductId(id){
 
 function getProductIdToUpdate(id, name, price, description) {
     // Make sure the modal is shown before attempting to update values
-
+    document.getElementById('editName').value = name;
+    document.getElementById('editPrice').value = price;
+    document.getElementById('editDescription').value = description;
      document.getElementById('productIdToUpdate').value=id;
 
  
